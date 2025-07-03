eb26bfcfa83d4b094732b0e87039f5bce50ae42d
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-03 18:09:02 +1000

    Connect temperature sensor to Arduino and output readings in celsius to serial monitor

--- aquarium-monitor/aquarium-monitor.ino
+++ aquarium-monitor/aquarium-monitor.ino
@@ -1,18 +1,50 @@
+// Libraries required for DS18B20 temperature sensor
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Delay (ms) between sensor reads
+constexpr int SENSORS_READ_DELAY{1000};
+
+constexpr int TEMP_PIN{2};
 constexpr int TDS_PIN{A5};
+
+// Setup temperature sensor
+OneWire one_wire{TEMP_PIN};
+DallasTemperature sensors(&one_wire);
 
 void setup() {
   Serial.begin(9600);
+  sensors.begin();
 }
 
 void loop() {
+  // Read TDS
   int tds_analog{analogRead(TDS_PIN)};
   double tds_voltage{analog_to_voltage(tds_analog)};
   double tds_ppm{voltage_to_tds_ppm(tds_voltage)};
 
-  Serial.print("TDS (ppm): ");
-  Serial.println(tds_ppm, 0);
+  // Read temperature
+  sensors.requestTemperatures(); 
+  double temp_c = sensors.getTempCByIndex(0);
 
-  delay(1000);
+  // Print readings
+  print_tds(tds_ppm);
+  print_temp(temp_c);
+  Serial.println();
+
+  delay(SENSORS_READ_DELAY);
+}
+
+void print_tds(double tds_ppm) {
+  Serial.print("        TDS: ");
+  Serial.print(tds_ppm, 0);
+  Serial.println(" ppm");
+}
+
+void print_temp(double temp_c) {
+  Serial.print("Temperature: ");
+  Serial.print(temp_c, 1);
+  Serial.println(" °C");
 }
 
 double voltage_to_tds_ppm(double voltage) {
