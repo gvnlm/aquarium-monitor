60022f50d0367f319140c21a7d7a8934b6af18de
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-07 18:41:19 +1000

    Add Express route to handle test JSON readings sent from Arduino web client

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,12 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).end();
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
