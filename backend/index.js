9a49d9acc1261cdd9b16984a2306039267040c61
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-10 17:27:38 +1000

    Edit backend server log messages

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.get('/tdsReadings', async (req, res, next) => {
     const tdsReadings = await TdsReading.find({});
     return res.json(tdsReadings);
   } catch (err) {
-    console.error('Failed to query database.');
+    console.error('Failed to fetch TDS readings from database.');
     next(err);
   }
 });
@@ -32,7 +32,7 @@ app.get('/tempReadings', async (req, res, next) => {
     const tempReadings = await TempReading.find({});
     return res.json(tempReadings);
   } catch (err) {
-    console.error('Failed to query database.');
+    console.error('Failed to fetch temperature readings from database.');
     next(err);
   }
 });
@@ -41,13 +41,13 @@ app.post('/', async (req, res, next) => {
   const { tds_ppm, temp_c } = req.body || {};
 
   if (tds_ppm === undefined) {
-    console.error('"tds_ppm" property is missing');
-    return res.status(400).json({ error: '"tds_ppm" property is missing' });
+    console.error('Failed to save readings to database: "tds_ppm" property is missing.');
+    return res.status(400).json({ error: '"tds_ppm" property is missing.' });
   }
 
   if (temp_c === undefined) {
-    console.error('"temp_c" property is missing');
-    return res.status(400).json({ error: '"temp_c" property is missing' });
+    console.error('Failed to save readings to database: "temp_c" property is missing.');
+    return res.status(400).json({ error: '"temp_c" property is missing.' });
   }
 
   const newTdsReading = new TdsReading({ ppm: tds_ppm });
@@ -74,5 +74,5 @@ app.post('/', async (req, res, next) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}.`);
 });
