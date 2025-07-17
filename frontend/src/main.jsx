f97b7998289ddc770241e3bdedbf5649788ab53f
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-17 18:39:23 +1000

    Add normalize.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import 'normalize.css';
+
 document.title = 'Aquarium Monitor';
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
