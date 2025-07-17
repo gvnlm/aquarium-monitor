e8a72628812f19b2d609d71af2864535e3495c06
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-17 15:40:57 +1000

    Change default maximum number of data points displayed to 100

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   const [endDate, setEndDate] = useState(new Date());
 
   // Maximum number of readings (i.e., readings) displayed on each chart
-  const [maxNumOfReadings, setMaxNumOfReadings] = useState(30);
+  const [maxNumOfReadings, setMaxNumOfReadings] = useState(100);
 
   const [tdsReadings, setTdsReadings] = useState([]);
   const [tempReadings, setTempReadings] = useState([]);
