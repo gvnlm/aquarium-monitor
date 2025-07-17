5df56eedebc57fd3ec3a3c82008426dcb344f1b0
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-17 14:56:25 +1000

    Extract loadTdsReadings and loadTempReadings definitions from useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,31 +19,35 @@ const App = () => {
   const [tempReadings, setTempReadings] = useState([]);
 
   useEffect(() => {
-    const loadTdsReadings = async () => {
-      try {
-        const tdsReadings = await tdsReadingsService.getRange(startDate, endDate, maxNumOfReadings);
-        setTdsReadings(tdsReadings);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    const loadTempReadings = async () => {
-      try {
-        const tempReadings = await tempReadingsService.getRange(
-          startDate,
-          endDate,
-          maxNumOfReadings
-        );
-        setTempReadings(tempReadings);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    loadTdsReadings();
-    loadTempReadings();
+    loadReadings();
   }, [startDate, endDate, maxNumOfReadings]);
+
+  const loadReadings = async () => {
+    try {
+      await loadTdsReadings();
+      await loadTempReadings();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadTdsReadings = async () => {
+    try {
+      const tdsReadings = await tdsReadingsService.getRange(startDate, endDate, maxNumOfReadings);
+      setTdsReadings(tdsReadings);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadTempReadings = async () => {
+    try {
+      const tempReadings = await tempReadingsService.getRange(startDate, endDate, maxNumOfReadings);
+      setTempReadings(tempReadings);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="app">
