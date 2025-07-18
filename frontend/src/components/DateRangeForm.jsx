2eec21d6f4b4bb0f3e7e7e556cebd1f6a351e9e6
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-18 23:25:43 +1000

    Keep date form label and input field together

--- frontend/src/components/DateRangeForm.jsx
+++ frontend/src/components/DateRangeForm.jsx
@@ -6,40 +6,44 @@ import '../styles/DateRangeForm.css';
 const DateRangeForm = ({ startDate, endDate, onStartDateChange, onEndDateChange }) => {
   return (
     <div className="date-range-form">
-      <label htmlFor="start-date">From:</label>
-      <DatePicker
-        id="start-date"
-        className="date-picker"
-        selected={startDate}
-        onChange={(date) => {
-          if (date.getTime() !== startDate.getTime()) {
-            onStartDateChange(date);
-          }
-        }}
-        maxDate={new Date()}
-        showTimeSelect
-        timeIntervals={15}
-        dateFormat="d/MM/yy, h:mm aa"
-        popperPlacement="top-start"
-      />
+      <div className="date-form">
+        <label htmlFor="start-date">From:</label>
+        <DatePicker
+          id="start-date"
+          className="date-picker"
+          selected={startDate}
+          onChange={(date) => {
+            if (date.getTime() !== startDate.getTime()) {
+              onStartDateChange(date);
+            }
+          }}
+          maxDate={new Date()}
+          showTimeSelect
+          timeIntervals={15}
+          dateFormat="d/MM/yy, h:mm aa"
+          popperPlacement="top-start"
+        />
+      </div>
 
-      <label htmlFor="end-date">To:</label>
-      <DatePicker
-        id="end-date"
-        className="date-picker"
-        selected={endDate}
-        onChange={(date) => {
-          if (date.getTime() !== endDate.getTime()) {
-            onEndDateChange(date);
-          }
-        }}
-        minDate={startDate}
-        maxDate={new Date()}
-        showTimeSelect
-        timeIntervals={15}
-        dateFormat="d/MM/yy, h:mm aa"
-        popperPlacement="top-start"
-      />
+      <div className="date-form">
+        <label htmlFor="end-date">To:</label>
+        <DatePicker
+          id="end-date"
+          className="date-picker"
+          selected={endDate}
+          onChange={(date) => {
+            if (date.getTime() !== endDate.getTime()) {
+              onEndDateChange(date);
+            }
+          }}
+          minDate={startDate}
+          maxDate={new Date()}
+          showTimeSelect
+          timeIntervals={15}
+          dateFormat="d/MM/yy, h:mm aa"
+          popperPlacement="top-start"
+        />
+      </div>
     </div>
   );
 };
