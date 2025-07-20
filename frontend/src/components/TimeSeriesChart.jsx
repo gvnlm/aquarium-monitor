e25137a194cb485007fa64dcb9746ab9142ce4c4
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-20 13:16:08 +1000

    Show and label latest data points for charts

--- frontend/src/components/TimeSeriesChart.jsx
+++ frontend/src/components/TimeSeriesChart.jsx
@@ -10,6 +10,8 @@ import {
 
 import '../styles/TimeSeriesChart.css';
 
+const fontSize = 16;
+
 const TimeSeriesChart = ({ data, yDataKey, yAxisTitle, lineColour, title }) => {
   // Convert data's timestamp strings to numeric representation (since Recharts requires numeric values for
   // accurate time axis scaling)
@@ -18,11 +20,28 @@ const TimeSeriesChart = ({ data, yDataKey, yAxisTitle, lineColour, title }) => {
     timestamp: new Date(datum.timestamp).getTime(),
   }));
 
+  // Dot component for Recharts' LineChart - shows only the last data point, and labels it
+  const ShowLastDot = ({ cx, cy, payload, index }) => {
+    // If this isn't the last data point
+    if (index !== processedData.length - 1) {
+      return null;
+    }
+
+    return (
+      <g>
+        <text x={cx} y={cy + fontSize} textAnchor="middle" fill={lineColour}>
+          {payload[yDataKey]}
+        </text>
+        <circle cx={cx} cy={cy} r={2} fill={lineColour} />
+      </g>
+    );
+  };
+
   return (
     <div className="time-series-chart">
       <h1>{title}</h1>
       <ResponsiveContainer className="responsive-container" width="100%" height="100%">
-        <LineChart data={processedData}>
+        <LineChart data={processedData} margin={{ right: fontSize * 1.5 }}>
           <CartesianGrid strokeDasharray="3 3" />
 
           <XAxis
@@ -38,7 +57,7 @@ const TimeSeriesChart = ({ data, yDataKey, yAxisTitle, lineColour, title }) => {
             label={{ value: yAxisTitle, position: 'insideLeft', angle: -90, offset: 8 }}
           />
 
-          <Line type="basis" dataKey={yDataKey} stroke={lineColour} dot={false} />
+          <Line type="basis" dataKey={yDataKey} stroke={lineColour} dot={<ShowLastDot />} />
 
           <Tooltip labelFormatter={unixTimeMsToString} />
         </LineChart>
