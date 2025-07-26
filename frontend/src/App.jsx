f512c512981de01e704fd58bae2de242a507552e
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-26 16:45:56 +1000

    Change default date range to past 7 days

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import './styles/App.css';
 
 const App = () => {
   // Date range of readings to display
-  const [startDate, setStartDate] = useState(getDateXHoursAgo(24));
+  const [startDate, setStartDate] = useState(getDateXHoursAgo(24 * 7));
   const [endDate, setEndDate] = useState(new Date());
 
   // Maximum number of readings (i.e., readings) displayed on each chart
